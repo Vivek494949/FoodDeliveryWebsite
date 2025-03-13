1eba978711c8ee3a609f92db6b7413627fb41b59
Vivek494949 <tejasvaidya49@gmail.com>
2025-03-13 10:51:32 +0530

    testing 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,6 +17,9 @@ export async function middleware(request: NextRequest) {
 	// console.log("Current Path:", request.nextUrl.pathname);
 	// console.log("Session exists:", !!session);
 	
+	  
+
+
 	
 	
 	// console.log("Is Public Path:", isPublicPath);
